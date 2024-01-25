7022ca79d1f5d64b6188b43a5d703e3ed94030e8
xavgdt66 <xaviergodet66100@gmail.com>
2024-01-25 10:43:37 +0100

    avant la jointure revien user locataire

--- src/Controller/Admin/CustomDashboardController.php
+++ src/Controller/Admin/CustomDashboardController.php
@@ -24,7 +24,7 @@ class CustomDashboardController extends AbstractDashboardController
         // Logique pour récupérer les données, par exemple, le nombre d'utilisateurs
         $userCount = $this->userRepository->count([]); // le nombre d'utilisateurs
       $tenantCountLocataire = $this->userRepository->countUsersByRole('ROLE_LOCATAIRE'); // le nombre de locataire 
-      $tenantCountAgence = $this->userRepository->countUsersByRole('ROLE_AGENCE'); // le nombre d'agence 
+      $tenantCountAgence = $this->userRepository->countUsersByRole('ROLE_AGENCY'); // le nombre d'agence 
 
         // Renvoyer la vue personnalisée avec les données
         return $this->render('admin/statisticalUser.html.twig', [
